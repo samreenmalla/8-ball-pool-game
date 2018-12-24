f9d216768cdc8bc90cb45d633996e0b8892d1772
samreenmalla <samreen.malla@gmail.com>
2018-12-24 15:08:35 +0545

    change background color

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,17 +1,16 @@
-function Canvas2D(){
+function Canvas2D() {
   var that = this;
-  this.canvas = document.getElementById('canvas');
-  canvas.setAttribute('width', '1500');
-  canvas.setAttribute('height', '825');
-  this.ctx = this.canvas.getContext('2d');
+  this.canvas = document.getElementById("canvas");
+  canvas.setAttribute("width", "1500");
+  canvas.setAttribute("height", "825");
+  this.ctx = this.canvas.getContext("2d");
 }
 
-Canvas2D.prototype.clear = function(){
+Canvas2D.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
+};
 
-Canvas2D.prototype.drawImage = function(image,position,origin,width,height,rotation = 0){
-
+Canvas2D.prototype.drawImage = function(image, position, origin, width, height, rotation = 0) {
   // if(!position){
   //   position = new Vector();
   // }
@@ -19,13 +18,11 @@ Canvas2D.prototype.drawImage = function(image,position,origin,width,height,rotat
   // if(!origin){
   //   origin = new Vector();
   // }
-
   this.ctx.save();
-  this.ctx.translate(position.x,position.y);
+  this.ctx.translate(position.x, position.y);
   this.ctx.rotate(rotation);
-  this.ctx.drawImage(image, -origin.x,-origin.y,width,height);
+  this.ctx.drawImage(image, -origin.x, -origin.y, width, height);
   this.ctx.restore();
-}
+};
 
 let Canvas = new Canvas2D();
-
